a39dd5746026802c3322eb819b1caf949bd97694
unknown <hussain.qurain@outlook.com>
2023-03-06 11:06:58 +0300

    added user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,6 +5,7 @@
 //     username varchar(30) NOT NULL,
 //     password_digest varchar
 // );
+// @ts-ignore
 import client from "../database";
 
 export type User {
@@ -16,8 +17,71 @@ export type User {
 }
 
 export class UserStore {
-    async index(): Promise<User[]> {
-        const conn = await client.connect();
-        const sql = 'SELECT * FROM users'
+    async indexUser(): Promise<User[]> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM users';
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        }
+        catch (err){
+            throw new Error(`Could not get users: ${err}`);
+        }
     }
+    async showUser(id: number): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM users where id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+
+        }
+        catch (err){
+            throw new Error(`Could not get the specified user: ${err}`);
+        }
+    }
+    async createUser(u: User): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'INSERT INTO users (first_name, last_name, username, password_digest) VALUES ($1, $2, $3, $4) RETURNING *';
+            const result = await conn.query(sql, [u.first_name, u.last_name, u.username, u.password_digest]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not create user ${u}: ${err}`);
+        }
+    }
+    async editUser(u: User): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'UPDATE users SET first_name = ($2), last_name = ($3), username = ($4) WHERE id = ($1) RETURNING *';
+            const result = await conn.query(sql, [u.id, u.first_name, u.last_name, u.username, u.password_digest]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not update user ${u}: ${err}`);
+        }
+    }
+    async deleteUser(id: number): Promise<User> {
+        try{
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'DELETE from users where id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not delete user: ${err}`);
+        }
+    }
+
 }
