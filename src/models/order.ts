439c45e25b201daf0661c287ea94bb0aa712bf82
unknown <hussain.qurain@outlook.com>
2023-03-06 12:47:28 +0300

    added order model

---
+++ src/models/order.ts
@@ -0,0 +1,77 @@
+// @ts-ignore
+import client from "../database";
+
+export type Order = {
+    id: number;
+    status: string;
+    user_id: number;
+}
+
+export class OrderStore {
+    async indexOrders(): Promise<Order[]> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM orders';
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        }
+        catch (err){
+            throw new Error(`Could not get orders: ${err}`);
+        }
+    }
+    async showOrder(id: number): Promise<Order> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM orders where id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+
+        }
+        catch (err){
+            throw new Error(`Could not get the specified order: ${err}`);
+        }
+    }
+    async createOrder(o: Order): Promise<Order> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'INSERT INTO orders (status, user_id) VALUES ($1, $2) RETURNING *';
+            const result = await conn.query(sql, [o.status, o.user_id]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not create order ${o}: ${err}`);
+        }
+    }
+    async editOrder(o: Order): Promise<Order> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'UPDATE orders SET status = ($2), user_id = ($3) WHERE id = ($1) RETURNING *';
+            const result = await conn.query(sql, [o.id, o.status, o.user_id]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not update order ${o}: ${err}`);
+        }
+    }
+    async deleteOrder(id: number): Promise<Order> {
+        try{
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'DELETE FROM orders WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not delete order: ${err}`);
+        }
+    }
+}
