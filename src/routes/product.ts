7afa7b57a1fbd5da704e423cfeaa427826608920
unknown <hussain.qurain@outlook.com>
2023-03-10 19:40:57 +0300

    products create, update, and delete routes require authorization

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { ProductHandler } from "../handlers/product";
-
+import verifyToken from "../middleware/authorization";
 const ProductRouter = Router();
 const Handler = new ProductHandler();
 
 ProductRouter.get('/', Handler.index);
 ProductRouter.get('/:id', Handler.show);
-ProductRouter.post('/create', Handler.create);
-ProductRouter.put('/:id', Handler.update);
-ProductRouter.delete('/:id', Handler.destroy);
+ProductRouter.post('/create', verifyToken, Handler.create);
+ProductRouter.put('/:id', verifyToken, Handler.update);
+ProductRouter.delete('/:id', verifyToken, Handler.destroy);
 
 export default ProductRouter;
