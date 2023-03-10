a0328a487905902b1944a51811b7a5fe60f13aab
unknown <hussain.qurain@outlook.com>
2023-03-10 19:43:58 +0300

    Orders routes require authorization

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { OrderHandler } from "../handlers/order";
-
+import verifyToken from "../middleware/authorization";
 const OrderRouter = Router();
 const Handler = new OrderHandler();
+
+OrderRouter.use(verifyToken);
 
 OrderRouter.get('/user_id/:user_id', Handler.index);
 OrderRouter.get('/:id', Handler.show);
