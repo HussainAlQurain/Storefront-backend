5a151f9f1f139ba0c25e89efd9ade6579ad283ee
unknown <hussain.qurain@outlook.com>
2023-03-10 19:38:18 +0300

    added authorization for some of the user routes

---
+++ dist/middleware/authorization.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const verifyToken = (req, res, next) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        const token = authorizationHeader ? authorizationHeader.split(' ')[1] : '';
+        res.locals.user = jsonwebtoken_1.default.verify(token, process.env.TOKEN_SECRET);
+        next();
+    }
+    catch (error) {
+        res.status(401);
+    }
+};
+exports.default = verifyToken;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { UserHandler } from "../handlers/users";
+import verifyToken from "../middleware/authorization";
 
 const UserRouter = Router();
 const Handler = new UserHandler();
 
-UserRouter.get('/', Handler.index);
-UserRouter.get('/:id', Handler.show);
+UserRouter.get('/', verifyToken, Handler.index);
+UserRouter.get('/:id', verifyToken, Handler.show);
 UserRouter.post('/create', Handler.create);
 UserRouter.post('/login', Handler.authenticate);
-UserRouter.put('/:id', Handler.update);
-UserRouter.delete('/:id', Handler.destroy);
+UserRouter.put('/:id', verifyToken, Handler.update);
+UserRouter.delete('/:id', verifyToken, Handler.destroy);
 
 export default UserRouter;
