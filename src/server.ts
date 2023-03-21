7a7eb19c0f4a8c66e6fd1302372d6d5b635857bd
unknown <hussain.qurain@outlook.com>
2023-03-21 14:09:54 +0300

    changed origin for angular port 4200

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import cors from 'cors';
 import routes from './routes';
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const address: string = "http://localhost:3000"
 
 const corsOptions = {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:4200',
     optionsSuccessStatus: 200
 }
 
